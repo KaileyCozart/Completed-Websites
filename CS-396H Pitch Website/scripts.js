25f05c095a856bc4cc2c06e127214bfa08567853
KaileyCozart <kaileync@comcast.net>
2018-02-05 22:39:28 -0800

    JS to play around with, added kolbies picture

--- CS-396H Pitch Website/scripts.js
+++ CS-396H Pitch Website/scripts.js
@@ -1,3 +1,18 @@
 window.onload = function() {
     document.getElementById("fade").className += " appear";
 };
+
+var imgArray = new Array();
+
+imgArray[0] = new Image();
+imgArray[0].src = 'images/cooking7.jpg';
+
+imgArray[1] = new Image();
+imgArray[1].src = 'images/cooking12.jpg';
+
+function arrayFunction() {
+    var newGallery = document.getElementsByClassName("screenshotsDiv").backgroundImage = imgArray.src;
+    for (i = 0; i < imgArray.length; i++) {
+        newGallery.appendChild(imgArray[i]);
+    }
+};
